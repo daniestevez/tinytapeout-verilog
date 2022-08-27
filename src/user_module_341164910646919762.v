f781922cde0dfde6ec01ae97551bf8e497329b50
Daniel Estévez <daniel@destevez.net>
2022-08-27 22:38:54 +0200

    replace sum by full-adder and half-adder sky130 cells

--- src/user_module_341164910646919762.v
+++ src/user_module_341164910646919762.v
@@ -38,9 +38,13 @@ module fibonacci_341164910646919762
    assign value = a;
    reg [15:0]    b;
 
+   wire [15:0]   sum;
+   adder_341164910646919762 #(.WIDTH(16)) adder
+     (.a(a), .b(b), .sum(sum));
+
    always @(posedge clk or negedge rstn) begin
       a <= b;
-      b <= a + b;
+      b <= sum;
 
       if (!rstn) begin
          a <= 1'b0;
@@ -48,3 +52,29 @@ module fibonacci_341164910646919762
       end
    end
 endmodule // fibonacci_341164910646919762
+
+module adder_341164910646919762
+  #(
+    parameter WIDTH = 16
+    )
+   (
+    input wire [WIDTH-1:0] a,
+    input wire [WIDTH-1:0] b,
+    output wire [WIDTH-1:0] sum
+    );
+
+   wire [WIDTH-1:0]         cout;
+
+   sky130_fd_sc_hd__ha_1 add_first
+     (.A(a[0]), .B(b[0]), .COUT(cout[0]), .SUM(sum[0]),
+      .VPWR(1'b1), .VGND(1'b0));
+
+   genvar                   i;
+   generate for (i = 1; i < WIDTH; i = i + 1) begin
+      sky130_fd_sc_hd__fa_1 add_rest
+                     (.A(a[i]), .B(b[i]), .CIN(cout[i-1]),
+                      .COUT(cout[i]), .SUM(sum[i]),
+                      .VPWR(1'b1), .VGND(1'b0));
+   end
+   endgenerate
+endmodule // adder_341164910646919762
